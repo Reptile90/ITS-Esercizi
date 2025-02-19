02cb279c3544ea4a3c939397de5cfe661648430c
Reptile90 <stefano.reali1990@gmail.com>
2025-02-19 18:16:10 +0100

    commit

--- Lezione_02/esercizio 2.3.py
+++ Lezione_02/esercizio 2.3.py
@@ -1,4 +1,4 @@
-'''ersonal Message: Use a variable to represent a person’s name, and print a message to that person. 
+'''Personal Message: Use a variable to represent a person’s name, and print a message to that person. 
 Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”'''
 
 name:str = "Marcel"
