5e7669f079783cdfa19d79df9307b7a0bd9f7e90
Reptile90 <stefano.reali1990@gmail.com>
2025-02-19 17:58:05 +0100

    commit

--- Lezione_02/esercizio 1.4.py
+++ Lezione_02/esercizio 1.4.py
@@ -6,3 +6,15 @@
 4'''
 
 print("2 \n0 \n2 \n5")
+
+
+n = int(input("inserisci un numero a 4 cifre"))
+m = n // 1000
+c = n // 100 - m * 10
+d = n // 10 - m *100 - c*10
+u = n - m * 1000 - c*100 - d*10
+
+print(m)
+print(c)
+print(d)
+print(u)
