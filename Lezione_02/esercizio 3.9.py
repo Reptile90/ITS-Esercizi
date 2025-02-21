68488f94bd0dc41c04a88c3866bab96195f52a42
Reptile90 <stefano.reali1990@gmail.com>
2025-02-21 14:44:36 +0100

    commit

--- Lezione_02/esercizio 3.9.py
+++ Lezione_02/esercizio 3.9.py
@@ -3,7 +3,9 @@ use len() to print a message indicating the number of people you’re inviting t
 
 guest_list:list = ["Anthony Kiedis", "John Frusciante", "Michael Balzary", "Chad Smith"]
 
-for guest in guest_list:
-    print(f"Dear {guest}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[0]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[1]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[2]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[3]}, i would be honored to have you join me for a dinner")
 
-    print(f"\nI am inviting {len(guest_list)} people to dinner.")
+print(f"\nI am inviting {len(guest_list)} people to dinner.")
