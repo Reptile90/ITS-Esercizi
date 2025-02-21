def854e8c202ac39e0526b300930240da8e6d058
Reptile90 <stefano.reali1990@gmail.com>
2025-02-21 14:40:45 +0100

    commit

---
+++ Lezione_02/esercizio 3.10.py
@@ -0,0 +1,51 @@
+'''Every Function: Think of things you could store in a list.
+For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+Write a program that creates a list containing these items and then uses each function introduced in this chapter at least once.'''
+
+lista = ["inglese", "Italiano", "Tedesco", "Spagnolo", "Francese"]
+
+
+lista.append("Giapponese")
+print(lista)
+
+
+lista.insert(3, "Greco")
+print(lista)
+
+
+lista.remove("Greco")
+print(lista)
+
+
+lista.insert(4, "Albanese")
+
+
+lista.sort()
+print(lista)
+
+
+lista.reverse()
+print(lista)
+
+
+lista.append("Rumeno")
+
+
+lista2 = lista.copy()
+print(lista2)
+
+
+lista2.reverse()
+print(lista2)
+
+
+lista.extend(lista2)
+
+
+lista.sort()
+print(lista)
+
+
+print(len(lista))
+
+
--- Lezione_02/esercizio 3.7.py
+++ Lezione_02/esercizio 3.7.py
@@ -8,15 +8,19 @@ Print your list to make sure you actually have an empty list at the end of your 
 
 guest_list:list = ["Anthony Kiedis", "John Frusciante", "Michael Balzary", "Chad Smith"]
 
-for guest in guest_list:
-    print(f"Dear {guest}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[0]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[1]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[2]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[3]}, i would be honored to have you join me for a dinner")
 
 print("\nUnfortunately, John Frusciante cannot come to dinner.\n")
 
 guest_list[1] = "Dave Navarro"
 
-for guest in guest_list:
-    print(f"Dear {guest}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[0]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[1]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[2]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[3]}, i would be honored to have you join me for a dinner")
 
 print("\nI found a bigger table, so we can invite more people!\n")
 
@@ -24,8 +28,10 @@ guest_list.insert(0,"Misha Mansoor")
 guest_list.insert(3, "Mark Holcomb")
 guest_list.append("Jake Bowen")
 
-for guest in guest_list:
-    print(f"Dear {guest}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[0]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[1]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[2]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[3]}, i would be honored to have you join me for a dinner")
 
 
 print("\nUnfortunately, the new dinner table won't arrive in time, and I can invite only two people to dinner.\n")
@@ -34,8 +40,10 @@ while len(guest_list) > 2:
     removed_guest = guest_list.pop()
     print(f"Sorry, {removed_guest}, but I can't invite you to dinner.")
 
-for guest in guest_list:
-    print(f"Dear {guest}, you are still invited to dinner.")
+print(f"Dear {guest_list[0]}, i would be honored to have you join me for a dinner")
+print(f"Dear {guest_list[1]}, i would be honored to have you join me for a dinner")
+
+
 
 del guest_list[:]
 print(f"\nFinal guest list: {guest_list}")
