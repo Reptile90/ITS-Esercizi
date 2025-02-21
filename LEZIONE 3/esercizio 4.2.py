ec2a2c9c8674416b6efe47b01c79c19724957047
Reptile90 <stefano.reali1990@gmail.com>
2025-02-21 19:27:41 +0100

    commit

---
+++ LEZIONE 3/esercizio 4.2.py
@@ -0,0 +1,14 @@
+'''Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+• Modify your program to print a statement about each animal, such as A dog would make a great pet.
+• Add a line at the end of your program, stating what these animals have in common. You could print a sentence, such as Any of these animals would make a great pet'''
+
+animals:list[str] = ["Lion", "Tiger", "Pantera", "Cheetah"]
+
+for felino in animals:
+    print(animals)
+
+for felino in animals:
+    print(f"I like {animals}")
+
+print(f"All of this animals {animals} live in the jungle")
+
