bca8bb4700fa8f3d58f3b431123085f5dddae87d
Reptile90 <stefano.reali1990@gmail.com>
2025-02-21 14:47:22 +0100

    commit

--- Lezione_02/esercizio 3.10.py
+++ Lezione_02/esercizio 3.10.py
@@ -8,43 +8,32 @@ lista = ["inglese", "Italiano", "Tedesco", "Spagnolo", "Francese"]
 lista.append("Giapponese")
 print(lista)
 
-
 lista.insert(3, "Greco")
 print(lista)
-
 
 lista.remove("Greco")
 print(lista)
 
-
 lista.insert(4, "Albanese")
-
 
 lista.sort()
 print(lista)
-
 
 lista.reverse()
 print(lista)
 
-
 lista.append("Rumeno")
-
 
 lista2 = lista.copy()
 print(lista2)
 
-
 lista2.reverse()
 print(lista2)
 
-
 lista.extend(lista2)
-
 
 lista.sort()
 print(lista)
-
 
 print(len(lista))
 
