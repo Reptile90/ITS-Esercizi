457731575fbdbff1991561c6c0e4f30d06f394a0
Reptile90 <stefano.reali1990@gmail.com>
2025-02-24 03:55:15 +0100

    commit

--- COMPITO/esercizio 5.py
+++ COMPITO/esercizio 5.py
@@ -7,14 +7,14 @@ Scrivere un programma che:
     Mostri quanti buoni sconto avanzano al termine dell'acquisto.'''
 
 
-
+#inizializzo le variabili
 barretta:int = 0
 
 buono_sconto:int = 0
 
 costo_barretta:int = 1
 
-
+#inizializzo il primo ciclo per far inserire l'importo e incrementare il numero di barrette e buoni sconto
 while True:
     n:int = int(input("Digita l'importo disponibile: "))
     if n == 0:
@@ -23,11 +23,12 @@ while True:
     barretta += n // costo_barretta
     buono_sconto += n // costo_barretta
 
-  
+    #inizializzo il secondo ciclo per verificare quante barrette totali e quanti buoni sconto rimangono
     while buono_sconto >= 6:
         barretta_omaggio:int = buono_sconto // 6
         barretta += barretta_omaggio
         buono_sconto = buono_sconto % 6
+    #stampo il risultato
     print(f"Hai acquistato {barretta} barrette")
     print(f"I buoni sconto rimanenti {buono_sconto}")
 
--- COMPITO/esercizio 6.py
+++ COMPITO/esercizio 6.py
@@ -2,14 +2,18 @@
 
 Il programma deve gestire anche il caso in cui n1 > n2, calcolando comunque il prodotto correttamente.'''
 
+#inizializzo le variabili
 n1 = int(input("Digita un numero intero: "))
 n2 = int(input("Digita un altro numero intero: "))
 
+#inizializzo il ciclo per la verifica dei due numeri
 if n1 > n2:
   n1,n2 = n2,n1
 prodotto=1
+
+#inizializzo il secondo ciclo per eseguire il prodotto
 for i in range(n1,n2):
 
   prodotto *= i
-
+#stampo il risultato
 print(f"il prodotto dei numeri compresi tra {n1} e {n2} è {prodotto}")
