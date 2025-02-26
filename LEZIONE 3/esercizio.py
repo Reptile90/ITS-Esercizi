077b4eaf878c1f1dee207256f1679e45381f2660
Reptile90 <stefano.reali1990@gmail.com>
2025-02-26 11:18:52 +0100

    commit

---
+++ LEZIONE 3/esercizio.py
@@ -0,0 +1,6 @@
+'''Utente inserisce in input una stringa contenente uno spazio. Bisogna stampare una stringa con la prima lettera di ogni parola in maiuscolo'''
+
+stringa:str = str(input("Inserisci una frase: ")).title()
+
+
+print(stringa)
