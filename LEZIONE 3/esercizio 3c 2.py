537024e36cc38a293723700174de3a1874369a47
Reptile90 <stefano.reali1990@gmail.com>
2025-02-26 12:30:22 +0100

    commit

--- LEZIONE 3/esercizio 3c 2.py
+++ LEZIONE 3/esercizio 3c 2.py
@@ -23,30 +23,34 @@ Inserisci il voto di laurea: 65
 Output: Voto non valido'''
 
 
-voto_laurea:int = int(input("Inserisci un voto compreso da 66 a 110: "))
-    
-    
+voto_laurea:int = input("Inserisci un voto compreso da 66 a 110: ")
+if voto_laurea.isnumeric():
+    voto_laurea = int(voto_laurea)
 
-match voto_laurea:
-    case voto_laurea if voto_laurea >= 106 and voto_laurea <= 110:
-            print("GPA 4.0")
-    case voto_laurea if voto_laurea >= 101 and voto_laurea <= 105:
-            print("GPA 3.7")
-    case voto_laurea if voto_laurea >= 96 and voto_laurea <= 100:
-            print("GPA 3.3")
-    case voto_laurea if voto_laurea >= 91 and voto_laurea <= 95:
-            print("GPA 3.0")
-    case voto_laurea if voto_laurea >= 86 and voto_laurea <= 90:
-            print("GPA 2.7")
-    case voto_laurea if voto_laurea >= 81 and voto_laurea <= 85:
-            print("GPA 2.3")
-    case voto_laurea if voto_laurea >= 76 and voto_laurea <= 80:
-            print("GPA 2.0")
-    case voto_laurea if voto_laurea >= 70 and voto_laurea <= 75:
-            print("GPA 1.7")
-    case voto_laurea if voto_laurea >= 66 and voto_laurea <= 69:
-            print("GPA 1.0")
-    case _:
-            print("Voto non valido")
+    
+    
+    match voto_laurea:
+        case voto_laurea if voto_laurea >= 106 and voto_laurea <= 110:
+                print("GPA 4.0")
+        case voto_laurea if voto_laurea >= 101 and voto_laurea <= 105:
+                print("GPA 3.7")
+        case voto_laurea if voto_laurea >= 96 and voto_laurea <= 100:
+                print("GPA 3.3")
+        case voto_laurea if voto_laurea >= 91 and voto_laurea <= 95:
+                print("GPA 3.0")
+        case voto_laurea if voto_laurea >= 86 and voto_laurea <= 90:
+                print("GPA 2.7")
+        case voto_laurea if voto_laurea >= 81 and voto_laurea <= 85:
+                print("GPA 2.3")
+        case voto_laurea if voto_laurea >= 76 and voto_laurea <= 80:
+                print("GPA 2.0")
+        case voto_laurea if voto_laurea >= 70 and voto_laurea <= 75:
+                print("GPA 1.7")
+        case voto_laurea if voto_laurea >= 66 and voto_laurea <= 69:
+                print("GPA 1.0")
+        case _:
+                print("Voto non valido")
+else:
+       print("Carattere non valido")
 
 
