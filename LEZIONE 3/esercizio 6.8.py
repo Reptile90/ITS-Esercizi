73d271213f63a8b78373802166bca4722d8c25bf
Reptile90 <stefano.reali1990@gmail.com>
2025-03-02 20:10:36 +0100

    commit

---
+++ LEZIONE 3/esercizio 6.8.py
@@ -0,0 +1,2 @@
+'''Pets: Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and as
+you do, print everything you know about each pet. '''
