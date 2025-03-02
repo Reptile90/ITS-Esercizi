155d189aea6c218e243adf0465d369c89de0c104
Reptile90 <stefano.reali1990@gmail.com>
2025-03-02 20:27:17 +0100

    commit

--- LEZIONE 3/esercizio 6.8.py
+++ LEZIONE 3/esercizio 6.8.py
@@ -1,2 +1,29 @@
 '''Pets: Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and as
 you do, print everything you know about each pet. '''
+
+
+pet1:dict[str]={
+    "Kind":"Cat",
+    "Owner":"Stefano"
+    }
+pet2:dict[str]={
+    "Kind":"Dog",
+    "Owner":"Lorenzo"
+}
+
+pet3:dict[str]= {
+    "Kind":"Parrot",
+    "Owner": "Marcel"
+}
+
+pet4:dict[str]= {
+    "Kind":"Rabbit",
+    "Owner": "Valentina"
+}
+
+pets= [pet1,pet2,pet3,pet4]
+
+
+for pet in pets:
+    print(f"\nPet Kind: {pet['Kind']}")
+    print(f"Owner: {pet['Owner']}")
