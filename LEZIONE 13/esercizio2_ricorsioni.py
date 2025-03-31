03c8255c2d26af104f1b6473e271f7c9476a0fb5
Reptile90 <stefano.reali1990@gmail.com>
2025-03-31 17:58:25 +0200

    commit

--- LEZIONE 13/esercizio2_ricorsioni.py
+++ LEZIONE 13/esercizio2_ricorsioni.py
@@ -2,10 +2,13 @@
 Scrivere una funzione ricorsiva compoundInterest che calcoli la somma presente sul conto dopo t mesi data una somma di partenza m.'''
 
 def compoundInterest(m,t)->float:
+    if t < 0:
+        print("Numero non valido")
+        return
     if t == 0:
         return m
     else:
         return 1.005 * compoundInterest(m, t - 1)
     
 
-print(compoundInterest(1000,350))
+print(f"{compoundInterest(1000,350):.2f}")
