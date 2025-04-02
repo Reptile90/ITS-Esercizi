3c10a2e427fd2a929e3e1f2867279e5c164c9819
Reptile90 <stefano.reali1990@gmail.com>
2025-04-02 17:01:37 +0200

    commit

--- LEZIONE 13/funzioni_ricorsive.py
+++ LEZIONE 13/funzioni_ricorsive.py
@@ -32,3 +32,18 @@ def recursiveSumInRange(a:int,b:int)->int:
         return int(b + recursiveSumInRange(a,b-1))
 
 print(recursiveSumInRange(5,10))
+
+
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+
+        return  int(fibonacci(n-1))+ (fibonacci(n-2))
+
+
+
+print(fibonacci(42))
---
+++ LEZIONE 18/esercizio1_eccezioni.py
@@ -0,0 +1,17 @@
+'''Safe Square Root: Write a function safe_sqrt(number) that calculates the square root of a number using math.sqrt(). Handle ValueError if the input is negative by returning an informative message.'''
+
+import math
+def safe_sqrt(number)->float:
+    try:
+        
+        print(math.sqrt(number))
+
+    except ValueError as error:
+        print(error)
+        print(f"The number must be positive")
+        
+
+
+
+
+(safe_sqrt(-1))
