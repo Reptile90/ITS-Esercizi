a5111c17b3ce961c88915c08a47eccfdc6d5b7f1
Reptile90 <stefano.reali1990@gmail.com>
2025-05-05 09:35:01 +0200

    commit

--- esercizi its tutti/LEZIONE 9 Regex+Lambda/RegEx/esercizio1.py
+++ esercizi its tutti/LEZIONE 9 Regex+Lambda/RegEx/esercizio1.py
@@ -1,16 +1,13 @@
 import re
 
 def is_integer(s)->bool:
-    return bool(re.fullmatch(r"-?\d+", s))     # ^. Garantisce che il match parta dall'inizio.
-                                                #-? Segno meno opzionale. Il simbolo ? indica che - può esserci o no.
-                                                #\d Una o più cifre. \d corrisponde a qualsiasi cifra da 0 a 9; + richiede almeno una
+    return bool(re.fullmatch(r"-?\d+", s))     
 
 
 
 
 
 
-
-print(is_integer("123"))     # True
-print(is_integer("-456"))    # True
-print(is_integer("12.3"))     # False
+print(is_integer("123"))     
+print(is_integer("-456"))    
+print(is_integer("12.3"))     
--- esercizi its tutti/LEZIONE 9 Regex+Lambda/RegEx/esercizio2.py
+++ esercizi its tutti/LEZIONE 9 Regex+Lambda/RegEx/esercizio2.py
@@ -4,4 +4,4 @@ def extract_emails(text) -> list:
     return re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", text)
 
 text = "Contattaci a info@azienda.com oppure support@help.org"
-print(extract_emails(text))  # ['info@azienda.com', 'support@help.org']
+print(extract_emails(text))  
