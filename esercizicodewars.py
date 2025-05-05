3aa0a160c165107b937b59ea1de0ee83e8ac4e9b
Reptile90 <stefano.reali1990@gmail.com>
2025-05-05 09:26:30 +0200

    caricamento esercizi its ordinati per argomento

---
+++ esercizicodewars.py
@@ -0,0 +1,20 @@
+
+
+def longest(a,b):
+    list1 = list(a)
+    list2 = list(b)
+    word1= ""
+    word2= ""
+    print(list1,list2)
+    for word in list1[-1], list2[-1]:
+        word1+=word
+        word2+=word
+    print(word1,word2)
+        
+
+
+
+
+
+
+longest("xyaabbbccccdefww","xxxxyyyyabklmopq")
