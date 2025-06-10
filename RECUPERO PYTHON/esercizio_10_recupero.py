6cf6c8cd460c260e708ba935fdc1e2e20d79af52
Reptile90 <stefano.reali1990@gmail.com>
2025-06-10 19:08:24 +0200

    update

--- RECUPERO PYTHON/esercizio_10_recupero.py
+++ RECUPERO PYTHON/esercizio_10_recupero.py
@@ -1,17 +1,18 @@
 def binary_search(lista, numero):
     start = 0 #imposto l'indice 0
     end = len(lista) - 1 #imposto l'indice finale
+    mid =  end // 2 
 
-    while start <= end: #fino a che start non sia uguale a end
-        mid = end // 2 #l'indice mid è uguale a start + end /2 restituendo un intero
+    while lista[start] <= lista[end]: #fino a che start non sarà uguale a end
+    
         if lista[mid] == numero: #se l'indice mid è uguale al target ritorna true
-            return f"Il numero {numero} è contenuto nella lista"
+            return True
         elif lista[mid] < numero: #altrimenti cerca sia a destra che a sinistra se presente il numero
             start = mid + 1
         else:
             end = mid - 1
 
-    return f"Il numero {numero} non è nella lista"
+        return False
 
 
 numeri = [1, 3, 5, 7, 9, 11, 13]
