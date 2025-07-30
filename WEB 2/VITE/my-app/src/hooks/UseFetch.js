73a9e40169f05644f2abdc6e6d21288f730a2bbc
Reptile90 <stefano.reali1990@gmail.com>
2025-07-30 11:37:15 +0200

    update react

---
+++ WEB 2/VITE/my-app/src/hooks/UseFetch.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+const UseFetch = () => {
+    const [data,setData] = useState([])
+    const [isLoading,setIsLoading]= useState(true);
+
+    useEffect(()=>{
+        (async()=>{
+            setIsLoading(true)
+            try{    
+                const {data}=await axios.get(url);
+                setData(data)
+            }catch(err){
+                console.log(err)
+            }setIsLoading(false)
+        })()
+    },[url])
+  return (data,isLoading)
+}
+
+export default UseFetch
