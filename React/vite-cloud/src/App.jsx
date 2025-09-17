c79c90d5b8a0a41ad74add66b2f08e0c67375446
Reptile90 <stefano.reali1990@gmail.com>
2025-09-17 19:08:13 +0200

    navbar funzionante

--- React/vite-cloud/src/App.jsx
+++ React/vite-cloud/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import './App.css'
-import UserAlbums from './UserAlbums'
 import Saluto from './esercizio_1/Saluto'
 import CardUtente from './esercizio_2/CardUtente'
 import MenuRistorante from './esercizio_3/MenuRistorante'
@@ -15,26 +13,41 @@ import TodoApp from './esercizio_11/TodoApp'
 import Navbar from './Navbar/Navbar'
 
 function App() {
+    const [esercizio, setEsercizio] = useState('')
 
-    const RenderCondizionale = 
+    const renderCondizionale = () => {
+        switch (esercizio){
+            case "Saluto":
+                return <Saluto/>
+            case "CardUtente":
+                return <CardUtente nome = "Stefano" email = "stefano.reali1990@gmail.com" imgUrl="https://images.unsplash.com/flagged/photo-1575555201693-7cd442b8023f?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+            case "MenuRistorante":
+                return<MenuRistorante></MenuRistorante>
+            case "Termostato":
+                return <Termostato></Termostato>
+            case "CampoRicerca":
+                return <CampoRicerca></CampoRicerca>
+            case "MessaggioSegreto":
+                return <MessaggioSegreto></MessaggioSegreto>
+            case "AggiornaTitolo":
+                return <AggiornaTitolo></AggiornaTitolo>
+            case "GalleriaFoto":
+                return <GalleriaFoto></GalleriaFoto>
+            case "ModuloContatti":
+                return <ModuloContatti></ModuloContatti>
+            case "BlogApp":
+                return <BlogApp></BlogApp>
+            case "TodoApp":
+                return <TodoApp></TodoApp>
+        }
+    }
+    
 
 
     return (
 
-        <> 
-            <Navbar></Navbar>
-            <Saluto/>
-            <CardUtente nome = "Stefano" email = "stefano.reali1990@gmail.com" imgUrl="https://images.unsplash.com/flagged/photo-1575555201693-7cd442b8023f?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-            <MenuRistorante></MenuRistorante>
-            <Termostato></Termostato>
-            <CampoRicerca></CampoRicerca>
-            <MessaggioSegreto></MessaggioSegreto>
-            <AggiornaTitolo></AggiornaTitolo>
-            <></>
-            <GalleriaFoto></GalleriaFoto>
-            <ModuloContatti></ModuloContatti>
-            <BlogApp></BlogApp>
-            <TodoApp></TodoApp>
+        <>  <Navbar onSetEsercizio={setEsercizio}></Navbar>
+        <div>{renderCondizionale()}</div>
 
 
         </>
